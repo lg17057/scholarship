f9d708200e9b842bc18c687a0d52a1f8f40292e3
rgdragon9 <lg17057@students.sacredheart.school.nz>
2023-04-10 16:52:13 +1200

    HTML pages up and running, 403 and 404 working

--- app.py
+++ app.py
@@ -1,6 +1,27 @@
-from flask import Flask
+# python -m flask run
+
+from flask import Flask, render_template
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return "Hello, Flask!"
+
+# Home page
+@app.route('/')
+def main():
+    return render_template('/index.html')
+
+@app.route('/device-logs')
+def index():
+    return render_template('device_logs.html')
+
+#Handles 403 errors -
+@app.errorhandler(403)
+def forbidden_error(error):
+    return render_template('error_page.html'), 403
+
+#Handles 404 errors -
+@app.errorhandler(404)
+def page_not_found_error(error):
+    return render_template('error_page.html'), 404
+
+if __name__ == '__main__':
+    app.run(debug="True")
