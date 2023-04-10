942209d0dd189ef8667136c06c62e165a38556c8
rgdragon9 <lg17057@students.sacredheart.school.nz>
2023-04-10 21:40:41 +1200

    New error catches created, and login/signup

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 # python -m flask run
 
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, redirect
 app = Flask(__name__, static_url_path='/static')
 
 
@@ -10,9 +10,42 @@ app = Flask(__name__, static_url_path='/static')
 def main():
     return render_template('/index.html')
 
+#device log page
 @app.route('/device-logs')
 def index():
-    return render_template('device_logs.html')
+    return render_template('/device_logs.html')
+
+#
+@app.route('/dev-admin')
+def dev_admin():
+    return render_template('/dev_admin.html')
+
+#
+@app.route('/admin')
+def admin():
+    return render_template('/admin.html')
+
+#
+@app.route('/login-page')
+def login_page():
+    return render_template('/login_page.html')
+
+#
+@app.route('/signup-page')
+def signup_page():
+    return render_template('/signup_page.html')
+
+
+############################################# Error catching
+#Handles 400 errors -
+@app.errorhandler(400)
+def bad_request_error(error):
+    return render_template('error_page.html'), 400
+
+#Handles 401 errors - 
+@app.errorhandler(401)
+def unauthorized_error(error):
+    return render_template('error_page.html'), 401
 
 #Handles 403 errors -
 @app.errorhandler(403)
@@ -24,5 +57,25 @@ def forbidden_error(error):
 def page_not_found_error(error):
     return render_template('error_page.html'), 404
 
+#Handles 405 errors -
+@app.errorhandler(405)
+def method_not_allowed_error(error):
+    return render_template('error_page.html'), 405
+
+#Handles 500 errors -
+@app.errorhandler(500)
+def internal_server_error(error):
+    db.session.rollback()
+    return render_template('error_page.html'), 500
+
+#Handles 503 errors -
+@app.errorhandler(503)
+def service_unavailable_error(error):
+    return render_template('error_page.html'), 503
+
+
+
+
+# Responsible for running the website
 if __name__ == '__main__':
     app.run(debug="True")
